47626d6b61d53ae9dcdc077584cb359f7251345b
kylemclaren <kyle.mclaren1@gmail.com>
2020-12-06 19:46:03 +0200

    create helloWorld deployment

--- main.tf
+++ main.tf
@@ -8,3 +8,50 @@ resource "kubernetes_namespace" "k8s-apps-namespace" {
   }
 }
 
+resource "kubernetes_deployment" "hello-world" {
+  metadata {
+    name      = "hello-world"
+    namespace = "apps"
+    labels = {
+      App = "helloWorldApp"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "helloWorldApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "helloWorldApp"
+        }
+      }
+      spec {
+        container {
+          image = "hello-world:1.0.0"
+          name  = "hello-world-app"
+
+          port {
+            container_port = 8080
+          }
+
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
