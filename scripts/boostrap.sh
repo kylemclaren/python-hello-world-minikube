650ae992bc2742c3bd244d8b8c09ac6825723298
kylemclaren <kyle.mclaren1@gmail.com>
2020-12-07 13:21:49 +0200

    install deps

--- scripts/boostrap.sh
+++ scripts/boostrap.sh
@@ -1,2 +1,42 @@
 #!/bin/bash
 
+PREFIX="/usr/local/bin"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+echo "Attempting to install minikube and assorted tools to $PREFIX"
+
+if ! command -v kubectl >/dev/null 2>&1; then
+    VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+    echo "Installing kubectl version $VERSION"
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/darwin/amd64/kubectl"
+    chmod +x kubectl
+    mv kubectl "$PREFIX"
+    echo "kubectl installed!"
+else
+    echo "kubetcl is already installed"
+fi
+
+if ! command -v minikube >/dev/null 2>&1; then
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64
+    chmod +x minikube
+    mv minikube "$PREFIX"
+    echo "minikube installed!"
+else
+    echo "minikube is already installed"
+fi
+
+# install Terraform
+
+echo "Attempting to install Terraform to $PREFIX"
+
+if ! command -v terraform >/dev/null 2>&1; then
+    curl -Lo terraform.zip https://releases.hashicorp.com/terraform/0.14.0/terraform_0.14.0_darwin_amd64.zip
+    echo "Unzipping Terraform..."
+    unzip terraform.zip
+    chmod +x terraform
+    mv terraform "$PREFIX"
+    echo "Terraform installed!"
+else
+    echo "Terraform is already installed"
+fi
+
